ee6276a4dda4972dee79eb959fad3650af96f2e1
soufianemir <soufiane.mir@gmail.com>
2026-08-09 17:07:00 +0200

    Remove invalid server action barrel

--- app/actions.ts
+++
@@ -1,6 +0,0 @@
-"use server";
-export * from "./actions/space";
-export * from "./actions/program";
-export * from "./actions/shopping";
-export * from "./actions/schedule";
-export * from "./actions/journal";
